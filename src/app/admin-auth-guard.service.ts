fb96cddd043bc35643dfe9889b5a583042528bad
wajayanath <wajayanath@gmail.com>
2019-06-24 22:47:48 +0530

    Hiding Admin links and logging out

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { AuthService } from './auth.service';
-import { map, switchMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { UserService } from './user.service';
 import { Observable } from 'rxjs';
 
@@ -13,8 +13,7 @@ export class AdminAuthGuard implements CanActivate {
   constructor(private auth: AuthService, private userService: UserService) { }
 
   canActivate(): Observable<boolean> {
-    return this.auth.user$
-      .pipe(switchMap(user => this.userService.get(user.uid).valueChanges()))
+      return this.auth.appUser$
       .pipe(map(appUser => appUser.isAdmin));
   }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,11 @@
-import { Observable } from 'rxjs'
+import { Observable, empty } from 'rxjs'
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
 import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './models/app-user';
+import { UserService } from './user.service';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,10 @@ import { ActivatedRoute } from '@angular/router';
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth,
+    private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
   }
 
@@ -23,6 +29,14 @@ export class AuthService {
 
   logout(){
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$() : Observable<AppUser> {
+    return this.user$
+      .pipe(switchMap(user => {
+        if (user) return this.userService.get(user.uid).valueChanges();
+        return empty();
+      }));
   }
 
 }
