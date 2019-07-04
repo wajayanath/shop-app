a9d3f53af1c809438f01db4a5bc855903ff01b18
wajayanath <wajayanath@gmail.com>
2019-07-04 10:09:42 +0530

    Revert "delete and filter product"
    This reverts commit 7fcc9bc4c3f083d5b5fbaa3b3a05af5d3b465d00.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,31 +1,16 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
-import { Subscription } from 'rxjs';
-import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: Product[];
-  filteredProducts: any[];
-  subscription: Subscription;
-
+export class AdminProductsComponent implements OnInit {
+  products$;
   constructor(private productService: ProductService) {
-    this.subscription = this.productService.getAll().subscribe(products => this.filteredProducts = this.products = products);
+    this.products$ = this.productService.getAll();
    }
-
-  filter(query: string) {
-    this.filteredProducts = (query) ?
-      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
-      this.products;
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 
   ngOnInit() {
   }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { Product } from 'src/app/models/product';
+
 
 @Injectable({
   providedIn: 'root'
@@ -15,18 +13,8 @@ export class ProductService {
     return this.db.list('/products').push(product);
   }
 
-  getAll(): Observable<Product[]> {
-    return this.db.list<Product>('/products')
-      .snapshotChanges()
-      .pipe(
-        map(changes =>
-          changes.map(c => {
-            const data = c.payload.val() as Product;
-            const id = c.payload.key;
-            return { id, ...data };
-          })
-        )
-      );
+  getAll() {
+    return this.db.list('/products').snapshotChanges();
   }
 
   get(productId) {
@@ -37,7 +25,4 @@ export class ProductService {
     return this.db.object('/products/'+productId).update(product);
   }
 
-  delete(productId) {
-    return this.db.object('/products/' + productId).remove();
-  }
 }
